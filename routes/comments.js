9624dcf4beba037a81676924cd9a9b2518160e07
hashsham123 <hashshamkhan8@gmail.com>
2023-08-09 17:44:24 +0530

    first commit

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
-import {addComment,deleteComment,getComment} from "../controllers/comment.js";
 import express from "express";
+import { addComment, deleteComment, getComment } from "../controllers/comment.js";
 import {verifyToken} from "../verifyToken.js"
 const router = express.Router();
-router.post("/",verifyToken,addComment);
-router.delete("/:id",verifyToken,deleteComment);
-router.get("/:videoId",getComment);
+
+router.post("/", verifyToken, addComment)
+router.delete("/:id", verifyToken, deleteComment)
+router.get("/:videoId", getComment)
 
 export default router;
